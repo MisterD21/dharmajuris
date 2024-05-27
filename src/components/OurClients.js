87303d6dc3f5d228a0db88c2266c1275cb3de32c
Dubey, Nandan <nandandubey44@gmail.com>
2024-05-27 17:26:29 +0530

    some changes 2

--- src/components/OurClients.js
+++ src/components/OurClients.js
@@ -1,32 +1,56 @@
-
 import React from 'react';
 import HeaderComponent from './commonComponent/HeaderComponent';
+
+// Define an array of client objects
+const clients = [
+  {
+    name: 'Client 1',
+    image: 'img/team-1.jpg',
+    description: 'Description of Client 1'
+  },
+  {
+    name: 'Client 2',
+    image: 'img/team-2.jpg',
+    description: 'Description of Client 2'
+  },
+  {
+    name: 'Client 3',
+    image: 'img/team-3.jpg',
+    description: 'Description of Client 3'
+  },
+  {
+    name: 'Client 5',
+    image: 'img/team-4.jpg',
+    description: 'Description of Client 2'
+  },
+  {
+    name: 'Client 5',
+    image: 'img/team-5.jpg',
+    description: 'Description of Client 3'
+  }
+];
 
 function OurClients() {
   return (
     <>
-
-        <HeaderComponent title="OurClients" />
-   
-        <div className="container-fluid py-5">
+      <HeaderComponent title="OurClients" />
+      <div className="container-fluid py-5">
         <div className="container py-5">
-            <div className="row">
-                    <img className="img-fluid rounded" src="img/about.jpg" alt=""/>
-
-                <h1>this is OurClients</h1>
-                
+          <div className="row">
+            {clients.map((client, index) => (
+              <div className="col-md-4" key={index}>
+                <div className="client-card">
+                  <img className="img-fluid rounded" src={client.image} alt={client.name} />
+                  <div className="client-info">
+                    <h4>{client.name}</h4>
+                    <p>{client.description}</p>
+                  </div>
                 </div>
-                <br/>
-                <div className="row">
-
-                <p>this is OurClients</p>
-                
-                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        </div>
-
-    
-
+      </div>
     </>
   );
 }
